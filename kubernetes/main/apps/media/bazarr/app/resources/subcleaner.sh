d5f79294bb6b9c36e4e8268666a035d7ab33883d
Darkfella91 <darkfella91@gmail.com>
2024-10-15 07:29:34 +0300

    test

--- kubernetes/main/apps/media/bazarr/app/resources/subcleaner.sh
+++ kubernetes/main/apps/media/bazarr/app/resources/subcleaner.sh
@@ -14,5 +14,5 @@ if [[ -n "$section" ]]; then
         --data-urlencode "path=$(dirname "$1")" \
         --data-urlencode "X-Plex-Token=${PLEX_TOKEN}" \
         --no-progress-meter \
-            "https://plex.${PUBLIC_DOMAIN}:32400/library/sections/${section}/refresh"
+            "https://plex.darkfellanetwork.com:32400/library/sections/${section}/refresh"
 fi

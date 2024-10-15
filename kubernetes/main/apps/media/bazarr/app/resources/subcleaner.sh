5946a3283db7969e985a81d29ba3766b3046d52a
Darkfella91 <darkfella91@gmail.com>
2024-10-15 07:34:35 +0300

    test

--- kubernetes/main/apps/media/bazarr/app/resources/subcleaner.sh
+++ kubernetes/main/apps/media/bazarr/app/resources/subcleaner.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-printf "Cleaning subtitles for '%s' ...\n" "$1"
-python3 /add-ons/subcleaner/subcleaner.py "$1" -s
+printf "Cleaning subtitles for '%s' ...\n" "$$1"
+python3 /add-ons/subcleaner/subcleaner.py "$$1" -s
 
 case $1 in
     *movies*) section="1";;
@@ -9,10 +9,10 @@ case $1 in
 esac
 
 if [[ -n "$section" ]]; then
-    printf "Refreshing Plex section '%s' for '%s' ...\n" "$section" "$(dirname "$1")"
+    printf "Refreshing Plex section '%s' for '%s' ...\n" "$$section" "$(dirname "$1")"
     /usr/bin/curl -I -X GET -G \
         --data-urlencode "path=$(dirname "$1")" \
-        --data-urlencode "X-Plex-Token=${PLEX_TOKEN}" \
+        --data-urlencode "X-Plex-Token=$${PLEX_TOKEN}" \
         --no-progress-meter \
-            "https://plex.darkfellanetwork.com:32400/library/sections/${section}/refresh"
+            "https://plex.${PUBLIC_DOMAIN}:32400/library/sections/$${section}/refresh"
 fi

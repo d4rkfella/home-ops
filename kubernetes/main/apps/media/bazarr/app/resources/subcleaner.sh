1839ff5bb5272209720706a967f3f17737d84af2
Darkfella91 <darkfella91@gmail.com>
2024-12-30 21:35:00 +0000

    (chore) disable flux substitution for configmap generator

--- kubernetes/main/apps/media/bazarr/app/resources/subcleaner.sh
+++ kubernetes/main/apps/media/bazarr/app/resources/subcleaner.sh
@@ -12,7 +12,7 @@ if [[ -n "$section" ]]; then
     printf "Refreshing Plex section '%s' for '%s' ...\n" "$section" "$(dirname "$1")"
     /usr/bin/curl -I -X GET -G \
         --data-urlencode "path=$(dirname "$1")" \
-        --data-urlencode "X-Plex-Token=$${PLEX_TOKEN}" \
+        --data-urlencode "X-Plex-Token=${PLEX_TOKEN}" \
         --no-progress-meter \
-            "https://plex.darkfellanetwork.com:32400/library/sections/$${section}/refresh"
+            "https://plex.darkfellanetwork.com:32400/library/sections/${section}/refresh"
 fi

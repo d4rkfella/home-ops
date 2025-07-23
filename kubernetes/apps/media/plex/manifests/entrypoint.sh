e8f4f3ff7a1b650994a1677624ff11aa271b2e7f
d4rkfella <georgi.panov@darkfellanetwork.com>
2025-07-23 11:20:22 +0000

    test

--- kubernetes/apps/media/plex/manifests/entrypoint.sh
+++ kubernetes/apps/media/plex/manifests/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-prefFile="${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}/Plex Media Server/Preferences.xml"
-
 # Remove pid file
 rm -f "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}/Plex Media Server/plexmediaserver.pid"
 
